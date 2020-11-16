ec2f678f003ce899f9ab1136e64fd4f0a9166eaa
Per Blåwiik <perj.sg@gmail.com>
2020-11-16 13:34:35 +0100

    Expanded the UI with input fields. Implemented a grid based sampling for generating scatter points.

--- scripts/scatter.py
+++ scripts/scatter.py
@@ -1,57 +1,72 @@
 import maya.cmds as cmds
 import maya.OpenMaya as om
+import functools
 import math
 
 from random import uniform as rand
+    
+def basicSampling( xMin, zMin, xMax, zMax, resolution, P ):
+   
+    sizeX = (xMax - xMin)
+    sizeZ = (zMax - zMin)
+    
+    delta = 0
+    dimX = 0
+    dimZ = 0
+    
+    # Adjust grid dimensions based on the largest side
+    if sizeX >= sizeZ:
+        dimX = resolution
+        dimZ = int((sizeZ/sizeX) * dimX)
+        
+        delta = sizeX / resolution
+    else:
+        dimZ = resolution
+        dimX = int((sizeX/sizeZ) * dimZ)
+        
+        delta = sizeZ / resolution
+        
+    # Generate a discrete grid by interpolating between min and max
+    xValues = []
+    zValues = []
+    for i in range(dimX):
+        xValues.append( lerp( xMin, xMax, i / float(dimX) ) )
+        
+    for i in range(dimZ):
+        zValues.append( lerp( zMin, zMax, i / float(dimZ) ) )
 
-def computeBoundingBox(fnMesh):
-    points = om.MPointArray()
-    fnMesh.getPoints(points, om.MSpace.kWorld)
-    vertices = [om.MVector(points[i]) for i in range(points.length())]
+    # Calculate a third of the cell size used for applying offsets
+    deltaHalf = delta * 0.5
     
-    xMin = vertices[0].x
-    yMin = vertices[0].y
-    zMin = vertices[0].z
+    # Sample xz-coordinates based on given probability P
+    samples = []
+    for j in range(1, dimZ):
+        for i in range(1, dimX):
+            if rand(1.0, 0.0) < P:
+                # Add a random offset to reduce regular sampling artifacts
+                x = xValues[i] + rand( deltaHalf, -deltaHalf )
+                z = zValues[j] + rand( deltaHalf, -deltaHalf )
+                samples.append((x, z))
     
-    xMax = vertices[1].x
-    yMax = vertices[1].y
-    zMax = vertices[1].z
-
-    for vertex in vertices:
-        if vertex.x < xMin:
-            xMin = vertex.x
-        if vertex.x > xMax:
-            xMax = vertex.x
-            
-        if vertex.y < yMin:
-            yMin = vertex.y
-        if vertex.y > yMax:
-            yMax = vertex.y
-            
-        if vertex.z < zMin:
-            zMin = vertex.z
-        if vertex.z > zMax:
-            zMax = vertex.z
-    
-    min = (xMin, yMin, zMin)
-    max = (xMax, yMax, zMax)
-    
-    return min, max
-
-def getFnMesh(meshName):
+    return samples
+                
+def lerp( a, b, t ):
+    return a * (1 - t) + b * t
+                
+def getFnMesh( meshName ):
     # Clear selection
-    cmds.select(cl=True)
-    om.MGlobal.selectByName(meshName)
+    cmds.select( cl=True )
+    om.MGlobal.selectByName( meshName )
     selectionList = om.MSelectionList()
     
-    om.MGlobal.getActiveSelectionList(selectionList)
+    om.MGlobal.getActiveSelectionList( selectionList )
     item = om.MDagPath()
-    selectionList.getDagPath(0, item)
+    selectionList.getDagPath( 0, item )
     item.extendToShape()
 
     return om.MFnMesh(item)
 
-def checkIntersection(fnMesh, rayOrigin, rayDirection):   
+def checkIntersection( fnMesh, rayOrigin, rayDirection ):   
     # No specified face IDs
     faceIds = None
     # No specified triangle IDs
@@ -84,94 +99,90 @@ def checkIntersection(fnMesh, rayOrigin, rayDirection):
     intersectionTolerance = 0.0001
 
     # Finds all intersections
-    intersectionFound = fnMesh.allIntersections(rayOrigin, rayDirection, faceIds, triangleIds, sortedIds,
+    intersectionFound = fnMesh.allIntersections( rayOrigin, rayDirection, faceIds, triangleIds, sortedIds,
                                   worldSpace, maxParam, biDirectionalTest,
                                   accelParams, sortIntersections, intersectionPoints,
                                   intersectionRayParams, intersectionFaces, intersectionTriangles, 
-                                  intersectBarycentrics1, intersectBarycentrics2, intersectionTolerance)
+                                  intersectBarycentrics1, intersectBarycentrics2, intersectionTolerance )
                                   
     intersectionPoint = (0,0,0)
     faceNormal = (0,0,0)
     
     if intersectionFound:
+        # The first intersection point in the array is the closest to the ray origin
         intersectionPoint = (intersectionPoints[0].x, intersectionPoints[0].y, intersectionPoints[0].z)
         
+        # Get the indices of the vertex normals of the intersection face
         normalIds = om.MIntArray()
-        fnMesh.getFaceNormalIds(intersectionFaces[0], normalIds)
+        fnMesh.getFaceNormalIds( intersectionFaces[0], normalIds )
         
+        # Get all normals of the mesh
         normals = om.MFloatVectorArray()
-        fnMesh.getNormals(normals,worldSpace)
+        fnMesh.getNormals( normals, worldSpace )
         
+        # Sum the vertex normals to approximate the face normal
         n = normalIds.length()
         sum = om.MFloatVector()
         for i in range(n):
             sum += normals[normalIds[i]]
-            print((normals[normalIds[i]].x,normals[normalIds[i]].y,normals[normalIds[i]].z))
             
         sum /= n
-        
         sum.normalize()
         faceNormal = (sum.x, sum.y, sum.z)
-        print(faceNormal)
-        
- 
+
     return intersectionFound, intersectionPoint, faceNormal
     
+def aimY(vec):
+    # Convert to OpenMaya vector
+    targetDir = om.MFloatVector(vec[0], vec[1], vec[2])
     
-def aimY(faceNormal):
-    b = om.MFloatVector(faceNormal[0], faceNormal[1], faceNormal[2])
-    
-    xyLength = math.sqrt(b.x*b.x+b.y*b.y)
-    vecLength = math.sqrt(b.x*b.x + b.y*b.y + b.z*b.z)
-    if xyLength == 0:
-        if b.x > 0:
-            zAngle = math.pi*0.5
-        else:
-            zAngle = -math.pi *0.5 
-    else:
-        zAngle = math.acos(b.y/xyLength)
-    
-    xAngle = math.acos(xyLength/vecLength)
-    
-    if b.z > 0:
-        xAngle = xAngle
-    else:
-        xAngle = -xAngle
-    
-    
-    
-    xAngleDeg = xAngle *180/math.pi
-    
-    
-    if b.x > 0:
-        zAngle = zAngle
-    else:
-        zAngle = -zAngle
-    
-    zAngleDeg = zAngle * 180/math.pi
-    
-    return xAngleDeg,zAngleDeg
-        
-            
-    
+    xyLength = math.sqrt( targetDir.x * targetDir.x + targetDir.y * targetDir.y )
 
+    # Make sure not to divide by zero (this is the case if targetDir is parallel to the z axis
+    if xyLength < 0.00001:
+        zAngle = math.pi * 0.5
+        if targetDir.x < 0:
+            zAngle *= -1.0
+    else:
+        zAngle = math.acos( targetDir.y / xyLength )
+    
+    if targetDir.x > 0:
+        zAngle *= -1.0
+    
+    xAngle = math.acos( xyLength / targetDir.length() )
+    if targetDir.z < 0:
+        xAngle *= -1.0
+    
+    # Convert from radian to degrees
+    xAngleDeg = xAngle * 180 / math.pi
+    zAngleDeg = zAngle * 180 / math.pi
 
-def generateScatterPoints(num_points=10):
+    return xAngleDeg, zAngleDeg
+
+def generateScatterPoints( resolutionField, probabilityField, surfaceOrientationCheckBox, locatorColorFieldGrp, *pArgs ):
     # Check if a mesh is selected
     selected = cmds.ls( sl=True )
     if len(selected) == 0:
         print("No mesh selected")
         return
     
+    # Get input field values from UI
+    resolution = cmds.intFieldGrp( resolutionField, query=True, value1=True )
+    probability = cmds.floatSliderGrp( probabilityField, query=True, value=True )
+    useSurfaceOrientation = cmds.checkBoxGrp( surfaceOrientationCheckBox, query=True, value1=True )
+    locatorColor = cmds.intFieldGrp( locatorColorFieldGrp, query=True, value=True )
+    
     # Get FnMesh of selected object to check ray imtersection
     fnMesh = getFnMesh(selected[0])
     
-    # Compute bounding volume
-    min, max = computeBoundingBox(fnMesh)
+    # Get bounding box coordinates
+    bbox = cmds.exactWorldBoundingBox( selected[0] )
     
-    for i in range( num_points ):
-        # Randomize ray origin
-        rayOrigin = om.MFloatPoint(rand(max[0], min[0]), max[1] + 10.0, rand(max[2], min[2]), 1.0)
+    # Generate sample coordinates
+    samples = basicSampling( bbox[0], bbox[2], bbox[3], bbox[5], resolution, probability )
+    
+    for coordinates in samples:
+        rayOrigin = om.MFloatPoint(coordinates[0], bbox[4] + 10.0, coordinates[1], 1.0)
         
         # Cast ray in negative y-direction
         rayDirection = om.MFloatVector(0, -1, 0)
@@ -187,48 +198,50 @@ def generateScatterPoints(num_points=10):
             # Set color of the locator
             shapeName = spaceLoc[0][0:7] + "Shape" + spaceLoc[0][7:]
             cmds.setAttr( "{}.overrideEnabled".format(shapeName), True )
-            cmds.setAttr( "{}.overrideColor".format(shapeName), 17 )
+            cmds.setAttr( "{}.overrideRGBColors".format(shapeName), True )
+            cmds.setAttr( "{}.overrideColorR".format(shapeName), locatorColor[0] )
+            cmds.setAttr( "{}.overrideColorG".format(shapeName), locatorColor[1] )
+            cmds.setAttr( "{}.overrideColorB".format(shapeName), locatorColor[2] )
             
             # Set position
             cmds.move( intersectionPoint[0], intersectionPoint[1], intersectionPoint[2], spaceLoc )
             
-            xAngleDeg,zAngleDeg = aimY(faceNormal)
-            
-            cmds.setAttr( "{}.rx".format(spaceLoc[0]), xAngleDeg )
-            cmds.setAttr( "{}.rz".format(spaceLoc[0]), -zAngleDeg )
-
-            
-            #cmds.rotate(xAngleDeg,0,0, spaceLoc )
-            #cmds.rotate(0,0,zAngleDeg, spaceLoc )
-
-            #cmds.rotete(zAngleDeg,spaceLoc)
-            
-            print(xAngleDeg)
-            print(zAngleDeg)
-        
+            # Adjust orientation based on the face normal
+            if useSurfaceOrientation:
+                xAngleDeg, zAngleDeg = aimY( faceNormal )
+                cmds.setAttr( "{}.rx".format(spaceLoc[0]), xAngleDeg )
+                cmds.setAttr( "{}.rz".format(spaceLoc[0]), zAngleDeg )
+    
     # Clear selection
-    cmds.select(cl=True)
+    cmds.select( cl=True )
+    
+    return
         
 def createModels():
+    # Check if a mesh is selected
+    selected = cmds.ls( sl=True )
+    if len(selected) == 0:
+        print("No mesh selected")
+        return
+
     # Get all space locators in the scene
     locators = cmds.ls( "locator*" )
     
     for i in range( len(locators) / 2 ):
-        # Get position of current locator
-        position = cmds.xform(locators[i], q=True, ws=True, t=True)
-        rotation = cmds.xform(locators[i], q=True, ws=True, ro=True)
+        # Get position and rotation of current locator
+        position = cmds.xform( locators[i], q=True, ws=True, t=True )
+        rotation = cmds.xform( locators[i], q=True, ws=True, ro=True )
 
-        
         # Create and move object to locator position
-        cube =  cmds.polyCube()[0]
-        cmds.move( position[0], position[1], position[2], cube, a=True )
-        cmds.rotate(rotation[0],rotation[1],rotation[2], cube )
+        cmds.instance( selected )
+        cmds.move( position[0], position[1], position[2], selected, a=True )
+        cmds.rotate( rotation[0], rotation[1], rotation[2], selected )
 
         # Remove locator
         cmds.delete( locators[i] )
         
     # Clear selection
-    cmds.select(cl=True)
+    cmds.select( cl=True )
 	
 	
 #----------------#
@@ -240,15 +253,39 @@ if cmds.window( 'scatterToolUI' , exists = True ) :
     cmds.deleteUI( 'scatterToolUI' ) 
 
 # Create window 
-toolWindow = cmds.window( 'scatterToolUI', title="Scatter Tool", width = 200,
-    mnb = False, mxb = False, sizeable = True, rtf = True )
+toolWindow = cmds.window( 'scatterToolUI', title="Scatter Tool", width=300, height=300 )
 
-general = cmds.rowColumnLayout ( nc = 1 , cw = ( 1 , 200 ) ) 
+cmds.columnLayout( adj=True )
 
-cmds.separator(h=10)
-cmds.button( 'Generate Scatter' , c = 'generateScatterPoints( )' ) 
-cmds.separator(h=10)
-cmds.button( 'Add Models' , c = 'createModels( )' ) 
-cmds.separator(h=10)
+cmds.separator( h=20, style="none" )
+cmds.text( label="Scatter Point Generator" )
+cmds.separator( h=10, style="none" )
+
+resolutionField = cmds.intFieldGrp( numberOfFields=1, label="Sample Resolution", value1=20 )
+
+cmds.separator( h=6, style="none" )
+
+probabilityField = cmds.floatSliderGrp( label="Probability Distribution", min=0.0, max=1.0, 
+                                        value=0.5, step=0.01, field=True )
+                                        
+cmds.separator( h=6, style="none" )
+
+surfaceOrientationCheckBox = cmds.checkBoxGrp( numberOfCheckBoxes=1, label="Adjust to Surface", value1=True )
+
+cmds.separator( h=6, style="none" )
+
+locatorColorFieldGrp = cmds.intFieldGrp( numberOfFields=3, label="Scatter Point Color", 
+                                         value1=255, value2=0, value3=0 )
+
+cmds.separator( h=10, style="none" )
+
+cmds.button( "Generate Scatter" , command=functools.partial( generateScatterPoints,
+                                                    resolutionField,
+                                                    probabilityField,
+                                                    surfaceOrientationCheckBox,
+                                                    locatorColorFieldGrp ) ) 
+cmds.separator( h=20 )
+
+cmds.button( "Add Models" , c = "createModels()" ) 
 
 cmds.showWindow( toolWindow ) 
